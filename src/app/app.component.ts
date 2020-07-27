26699a064de05cbbb6843e9d4c9fc64b108f6807
Matheus Soares <m.soares.ms@outlook.com>
2020-07-27 18:56:02 -0400

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
-
+import * as dom2img from 'dom-image';
+import { saveAs } from 'file-saver';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'div-image';
+
+  public saveImg() {
+    dom2img.toBlob(document.getElementById('card')).then((blob) => {
+      saveAs.saveAs(blob, 'img.png');
+    });
+  }
 }
